2a10e864a6870bec14140a34803cfdd9a483e996
saurabhkatkar <saurabhkatkar@users.noreply.github.com>
2019-01-04 16:17:06 +0530

    adding database connection

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const profile = require('./controllers/profile');
 const db=knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : '',
-    database : 'smart-brain'
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
